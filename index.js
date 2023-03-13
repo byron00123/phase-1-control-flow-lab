b754e878d054e6bd5b78881a446d8a7bb48dd297
Byron Omondi <omondibyron25@gmail.com>
2023-03-13 14:45:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  
+  if(distance <= 400){
+    return "This one is on me!";
+  }
+  else if(distance >= 2000 && distance <= 2499){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(distance >= 2500){
+    return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(Place){
   // Write your code here!
+  return(Place === 'NYC' ? "Ok, sounds good.":"No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
+  switch(Tip){
+    case "generous":
+    return "Thank you so much."
+    break;
+    case 'not as generous':
+    return  'Thank you.' 
+    break;
+    default:
+    return 'Bye.'  
+
+
+  }
+  
 }
